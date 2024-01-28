164eeb5a16d083fe6ba017509da9e6d216383f69
johnsiak <149271195+johnsiak@users.noreply.github.com>
2024-01-28 20:05:26 +0200

    Update Board.java

--- Part 2/Board.java
+++ Part 2/Board.java
@@ -1,8 +1,3 @@
-/*Ioannis Siakavaras, 10053, 6946937774, siakavari@ece.auth.gr
-  Christoforos Chatziantoniou, 10258, 6946495698, cchristofo@ece.auth.gr
-  Team 186
- */
-
 public class Board {
 	
 	private int N;				//the dimension of the labyrinth
